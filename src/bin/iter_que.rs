a1146fbbbb013ace5ea0cae2aa7aaeac61eceeca
Kshitij886 <kshitijkhatri437@gmail.com>
2026-02-09 15:37:29 +0545

    add .collect() function instead of looping

--- src/bin/iter_que.rs
+++ src/bin/iter_que.rs
@@ -2,12 +2,8 @@
 
 fn main() {
     let vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-    let vec_iter = vec.iter().filter(|x| *x % 2 != 0).map(|x| x * 2);
-    let mut new_vec = Vec::<u32>::new();
-    for i in vec_iter {
-        new_vec.push(i);
-    }
-    println!("{:?}", new_vec);
+    let new: Vec<u32> = vec.iter().filter(|x| *x % 2 != 0).map(|x| x * 2).collect();
+    println!("{:?}", new);
 }
 
 // write a logic to first filter all odd values then double each values and create a new vector
